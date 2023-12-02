cf22842172442733e51b7cd8d0cf3d5e59590647
Hyun Woo Choi <coladrop93@gmail.com>
2023-12-02 18:18:55 +0900

    added events on the charts

--- my-app/src/components/Chart.tsx
+++ my-app/src/components/Chart.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+// import { Tooltip } from "@mui/x-charts/LineChart";
+import { ChartsReferenceLine, ChartContainer } from "@mui/x-charts";
 import { LineChart } from "@mui/x-charts/LineChart";
 
 const years = [
@@ -34,25 +36,63 @@ const isAuthorKoreanHeritage = [
 ];
 
 const events = [
-  { date: new Date(2001, 8, 11), label: "September 11 attacks" },
+  {
+    date: new Date(2001, 8, 11),
+    label: "September 11 attacks",
+    align: "start",
+  },
   // Add other events as needed
-  { date: new Date(2004, 11, 2), label: "United States presidential election" },
-  { date: new Date(2005, 8, 29), label: "Hurricane Katrina" },
-  { date: new Date(2007, 4, 16), label: "Virginia Tech shooting" },
-  { date: new Date(2008, 11, 4), label: "United States presidential election" },
-  { date: new Date(2009, 0, 20), label: "Inauguration of Barack Obama" },
-  { date: new Date(2012, 10, 6), label: "United States presidential election" },
+  {
+    date: new Date(2004, 11, 2),
+    label: "United States presidential election",
+    align: "start",
+  },
+  { date: new Date(2005, 8, 29), label: "Hurricane Katrina", align: "middle" },
+  {
+    date: new Date(2007, 4, 16),
+    label: "Virginia Tech shooting",
+    align: "start",
+  },
+  {
+    date: new Date(2008, 11, 4),
+    label: "United States presidential election",
+    align: "middle",
+  },
+  {
+    date: new Date(2009, 0, 20),
+    label: "Inauguration of Barack Obama",
+    align: "end",
+  },
+  {
+    date: new Date(2012, 10, 6),
+    label: "United States presidential election",
+    align: "start",
+  },
   {
     date: new Date(2013, 7, 13),
     label: "Beginning of the Black Lives Matter movement",
+    align: "middle",
   },
-  { date: new Date(2016, 11, 8), label: "United States presidential election" },
-  { date: new Date(2020, 1, 20), label: "Covid-19 pandemic" },
-  { date: new Date(2020, 5, 26), label: "George Floyd protests" },
-  { date: new Date(2020, 10, 3), label: "United States presidential election" },
+  {
+    date: new Date(2016, 11, 8),
+    label: "United States presidential election",
+    align: "start",
+  },
+  { date: new Date(2020, 1, 20), label: "Covid-19 pandemic", align: "start" },
+  {
+    date: new Date(2020, 5, 26),
+    label: "George Floyd protests",
+    align: "middle",
+  },
+  {
+    date: new Date(2020, 10, 3),
+    label: "United States presidential election",
+    align: "end",
+  },
   {
     date: new Date(2021, 0, 6),
     label: "Storming of the United States Capitol",
+    align: "start",
   },
 ];
 
@@ -93,8 +133,94 @@ export default function Chart() {
         width={2000}
         height={700}
         margin={{ left: 70 }}
-      />
-      {/* <EventOverlay events={events} /> */}
+      >
+        {/* {events.map((event) => (
+          <ChartsReferenceLine
+            x={event.date}
+            lineStyle={{ stroke: "red" }}
+            labelAlign={event.align ? event.align : "start"}
+            label={event.label}
+          />
+        ))} */}
+        <ChartsReferenceLine
+          x={new Date(2001, 8, 11)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"start"}
+          label={"September 11 attacks"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2004, 11, 2)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"start"}
+          label={"United States presidential election"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2005, 8, 29)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"middle"}
+          label={"Hurricane Katrina"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2007, 4, 16)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"start"}
+          label={"Virginia Tech shooting"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2008, 11, 4)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"middle"}
+          label={"United States presidential election"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2009, 0, 20)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"end"}
+          label={"Inauguration of Barack Obama"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2012, 10, 6)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"start"}
+          label={"United States presidential election"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2013, 7, 13)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"middle"}
+          label={"Beginning of the Black Lives Matter movement"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2016, 11, 8)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"start"}
+          label={"United States presidential election"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2020, 1, 20)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"start"}
+          label={"Covid-19 pandemic"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2020, 5, 26)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"middle"}
+          label={"George Floyd protests"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2020, 10, 3)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"end"}
+          label={"United States presidential election"}
+        />
+        <ChartsReferenceLine
+          x={new Date(2021, 0, 6)}
+          lineStyle={{ stroke: "red" }}
+          labelAlign={"start"}
+          label={"Storming of the United States Capitol"}
+        />
+      </LineChart>
     </>
   );
 }
